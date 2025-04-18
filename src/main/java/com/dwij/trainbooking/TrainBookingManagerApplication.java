6586a0e26084326cfa5b5b4bd300de665b7437ae
Dwij Sukeshkumar Sheth <dwijhariket@gmail.com>
2025-04-18 03:30:50 +0000

    chore: Initial Structure commit for Train Booking Manager

---
+++ src/main/java/com/dwij/trainbooking/TrainBookingManagerApplication.java
@@ -0,0 +1,12 @@
+package com.dwij.trainbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainBookingManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainBookingManagerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dwij/trainbooking/TrainBookingManagerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dwij.trainbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainBookingManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
